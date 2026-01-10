1d65f66c6a9e9ece764ff64caa738cd3d61248e5
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-01-10 03:44:05 +0000

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"  // Treat all the codes as new JS Version
+
+// alert (4+5) we are using node js not browser so alert is not a corrrect syntax
+
+// Standards of Javascript are defined in ECMA script 
+
+// Primitive Data Types
+
+// Number data type => 2^53
+let age = 18
+// bignint
+// string
+let name = "Shri Ram"
+// boolean
+let isLoggedIn = true
+// null = Stand alone value , intentionally 
+// declaring a variable with null
+let a = null
+// undefined
+let b
+// symbol = unique
+
+console.log(typeof a)
+console.log(typeof b)
