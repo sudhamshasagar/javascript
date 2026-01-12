d64fe2c943adfd8440370fa224a401589681877b
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-01-12 16:28:47 +0000

    data type conversion

---
+++ dataTypeConversion.js
@@ -0,0 +1,25 @@
+let num = 33
+// console.log(typeof num)
+
+let a = null
+valueNumber = Number(a)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+//  "12" => 12
+// "12RAM" => NaN , type = Number
+// true => 1 false => 0 type= Number
+
+
+let isLoggedIn = " "
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// Empty string would be false
+// Empty string with space would be true
+
+let stringNumber = 5
+stringNumber = String(stringNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
