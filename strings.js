026f2643e8a47cb85278b37d096ccfa9e96bbb8f
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-01-18 05:48:17 +0000

    Intro on Strings

---
+++ strings.js
@@ -0,0 +1,11 @@
+const name = "Shri Ram"
+const repCount = 100
+
+// console.log(name +  repCount)
+// console.log(`Hello my name is ${name} and the repoCount is ${repCount}`)
+
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('r'))
+
