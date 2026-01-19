ef1fd08304594bd9e26f799209eeb9850a6b9fe1
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-01-19 11:54:10 +0000

    practice problems basics

---
+++ practice.js
@@ -0,0 +1,14 @@
+console.log(typeof typeof true)
+// typeof typeof will always be a string
+
+console.log(typeof("5" -2 + true))
+//  -ve enforces numeric type conversion thus the output will be 4 and type will be number
+
+console.log("5"+2+true)
+console.log("5"+2-true)
+
+console.log(null == undefined)
+console.log("shri ram".length)
+
+let name = "shri ram"
+name.
