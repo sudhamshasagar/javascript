40ae21592a19572eda04159cc3c6280720fd9a46
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-01-27 05:47:41 +0000

    module on dates in js

---
+++ dates.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate.toString())
+
+console.log(myDate.toDateString())
+// Month starts from 0
+let newDate = new Date(2026,0,26)
+console.log(newDate.toLocaleString())
