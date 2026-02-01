c2211adc3bccf3cab3f41c14ac9ecf993bdde49f
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-02-01 05:49:00 +0000

    functions with objects and arrays

--- functions.js
+++ functions.js
@@ -24,4 +24,26 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Sudhamsha"));
+// console.log(loginUserMessage("Sudhamsha"));
+
+function calculateCartPrice(...num1){
+    // When we need to past many operators we will make use of rest/spread operator
+    return num1
+}
+// console.log(calculateCartPrice(200,400,600))
+
+function anotherFunctions(val1, val2, ...num1){
+    return num1
+}
+// console.log(anotherFunctions(200,400,500,5000))
+
+const user = {
+    username: "sudhamsha",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+handleObject(user)
