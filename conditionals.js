68e6022386c75b0307ee0e3ad93cf5009d7d7b46
Sudhamsha Sagar <124686189+sudhamshasagar@users.noreply.github.com>
2026-02-07 06:52:00 +0000

    conditionals continued

--- conditionals.js
+++ conditionals.js
@@ -21,9 +21,54 @@ const balance = 1000
 if(balance > 500) console.log("true"); //Implicit scope
 
 if(balance < 500 ){
-    console.log("less than")
+    // console.log("less than")
 }else if (balance < 700){
-    console.log("less")
+    // console.log("less")
 }else{
-    console.log(`value is ${balance}`)
+    // console.log(`value is ${balance}`)
 }
+
+const month = "March"
+
+switch (month) {
+    case "jan":
+        // console.log("January");
+        break;
+    case "Feb":
+        // console.log("Febraury");
+        break;
+    case "March":
+        // console.log("March");
+        break;
+    case "April":
+        // console.log("April");
+        //break
+    default:
+        // console.log("Nothing Matched");
+        break;
+}
+
+// Falsy values
+// false, 0, -0 , BigInt 0n, Empty String, null, undefined, NaN 
+
+// truthy values
+// "0",'false', " ", [], {}, function()
+
+const userEmail = []
+
+if(userEmail.length === 0){
+    // console.log("Array is empty");
+    
+}
+
+const emtpyObj = {}
+
+if(Object.keys(emtpyObj).length ===0){
+    // console.log("Object also empty");
+
+}
+
+// console.log(typeof [])
+
+const array1 = ["Shri Ram"]
+// console.log(typeof array1);
